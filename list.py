2f0056246800efa4fcd784ee015d8d5d3d2e213f
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-14 17:41:52 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,13 @@
+fruits = ["mango", "orange", "banana"]
+
+print("my fruits:")
+print(fruits)
+
+fruits.append("apple")
+
+print("After adding apple:")
+print(fruits)
+
+fruits.remove("orange")
+print("after removing orange:")
+print(fruits)
