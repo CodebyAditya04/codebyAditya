88ccec326d32dcc9d4c1855d44f29b47d493f305
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-17 19:16:55 +0530

    Add files via upload

---
+++ dictionary_basic.py
@@ -0,0 +1,9 @@
+person = {
+	"name": "Aditya",
+	"age": "21",
+	"college": "xyz university",
+}
+
+print("name:", person ["name"])
+print("age:", person ["age"])
+print("college", person ["college"])
