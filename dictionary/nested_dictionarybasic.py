d3c8589f69ac005ca48530b2fb0be2e3678cc2df
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-17 21:34:29 +0530

    Add files via upload

---
+++ dictionary/nested_dictionarybasic.py
@@ -0,0 +1,9 @@
+students = {
+	"Aditya":{"grade":"A","age":21},
+	"Aditi":{"grade":"B","age":20}
+}
+print(students["Aditya"]["age"])
+print(students["Aditya"]["grade"])
+
+print(students["Aditi"]["age"])
+print(students["Aditi"]["grade"])
