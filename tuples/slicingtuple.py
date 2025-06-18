fabb2b63090c8d83ac9c4333385e02fcbe594d0e
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-18 19:16:12 +0530

    Add files via upload

---
+++ tuples/slicingtuple.py
@@ -0,0 +1,13 @@
+movies = ("interstellar", "fight club", "inception", "tennet", "dunkirk", "the prestige")
+
+print("All movies:")
+print(movies)
+
+print("\n first three movies:")
+print(movies[0:3])
+
+print("\n last two movies:")
+print(movies[-2:])
+
+print("\n every second movie:")
+print(movies[::2])
