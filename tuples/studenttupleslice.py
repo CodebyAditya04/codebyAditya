dd83ee4cf23a9c8e5f75c09300d4535f50361d99
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-18 19:48:38 +0530

    Add files via upload

---
+++ tuples/studenttupleslice.py
@@ -0,0 +1,13 @@
+students = ("Aditya", "Aditi", "Sneha", "Karan", "Avni", "Arnav", "Riya", "Kabir")
+
+print("All the students:")
+print(students)
+
+print("\n first 3 students:")
+print(students[:3])
+ 
+print("\n last 3 students:")
+print(students[-3:])
+
+print("\n every second student:")
+print(students[::2])
