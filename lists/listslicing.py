ee7dfb5265eb0a4b461660a7f0c9b1ae64d4e03a
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-19 17:23:21 +0530

    Add files via upload

---
+++ lists/listslicing.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "mango", "orange", "grape", "pineapple"]
+
+print("First 3 fruits:")
+print(fruits[0:3])  
+
+print("\nLast 2 fruits:")
+print(fruits[-2:])
+
+print("\nFruits from index 2 to 4:")
+print(fruits[2:5])
