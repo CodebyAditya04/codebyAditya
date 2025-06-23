866ca83a8bc099cf84dc6b41090fef91c1489e0d
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-23 23:55:48 +0530

    Add files via upload

---
+++ if-else recap/iflargenum.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+num3 = int(input("Enter the third number:"))
+
+if num1 > num2 and num1 > num3:                           #we have to add num1 > num3 because if we won't it would just consider num3 true as it is non-zero.
+	print("num1 is the largest")                          #also if statements check to see if a given condition is true so python will just see num1>num2 true and num3(non-zero) true.
+elif num2 > num1 and num2 > num3:
+	print("num2 is the largest")
+else:
+	print("num3 is the largest")
