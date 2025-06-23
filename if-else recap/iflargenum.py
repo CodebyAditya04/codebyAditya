76a55c7efad64bb986c611d39c6571f2b881de0c
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-23 23:58:22 +0530

    Update iflargenum.py

--- if-else recap/iflargenum.py
+++ if-else recap/iflargenum.py
@@ -4,7 +4,7 @@ num3 = int(input("Enter the third number:"))
 
 if num1 > num2 and num1 > num3:                           #we have to add num1 > num3 because if we won't it would just consider num3 true as it is non-zero.
 	print("num1 is the largest")                          #also if statements check to see if a given condition is true so python will just see num1>num2 true and num3(non-zero) true.
-elif num2 > num1 and num2 > num3:
+elif num2 > num1 and num2 > num3:                          #we need to write full condition for strong logic.
 	print("num2 is the largest")
 else:
 	print("num3 is the largest")
