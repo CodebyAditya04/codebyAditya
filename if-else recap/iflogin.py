bf0f4ba6ec655e865cc42359a601e92d41b9fe1e
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-24 14:05:11 +0530

    Update iflogin.py

--- if-else recap/iflogin.py
+++ if-else recap/iflogin.py
@@ -1,11 +1,11 @@
 username = input("Enter the username:")
-password = int(input("enter the password:"))
+password = input("Enter the password:"))
 
-if username == "admin" and password == 2004:
+if username == "admin" and password == "2004":
 	print("Login successful!")
-elif username != "admin" and password == 2004:
+elif username != "admin" and password == "2004":
 	print("Invalid username!")
-elif username == "admin" and password != 2004:
+elif username == "admin" and password != "2004":
 	print("Invalid password!")
 else:
 	print("Invalid credentials!")
