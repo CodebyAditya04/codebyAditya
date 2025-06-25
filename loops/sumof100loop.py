64c86d352f3a233e1ead79bf6109bb9a74af3161
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-25 16:31:03 +0530

    Add files via upload

---
+++ loops/sumof100loop.py
@@ -0,0 +1,9 @@
+i = 1
+total = 0
+
+while i <= 100:
+	print(i)
+	total += i
+	i += 1
+
+print("the total is:", total)
---
+++ loops/userinputloop.py
@@ -0,0 +1,5 @@
+i = int(input("Enter the number:"))
+
+while i >= 1:
+	print(i)
+	i -= 1
