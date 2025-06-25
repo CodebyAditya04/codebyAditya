664c064a91dc2f1d833174aa29669de45cc42af8
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-06-25 15:46:35 +0530

    Add files via upload

---
+++ loops/loop.py
@@ -0,0 +1,5 @@
+i = 1                                #start the counter at 1
+
+while i <= 3:                        #repeat the loop until i = 3
+	print("loop running",i)          #print the current loop number
+	i += 1                           #increase i by 1 each time 
---
+++ loops/loop2.py
@@ -0,0 +1,5 @@
+i = 5
+
+while i >= 1:
+	print("loop running", i)
+	i -= 1
