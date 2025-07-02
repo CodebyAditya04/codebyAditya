cacb41c34b5984f11bc8487ee406980a2d9fc52e
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-07-02 20:50:12 +0530

    Add files via upload

---
+++ loops/forloopeven.py
@@ -0,0 +1,4 @@
+N = int(input("Enter the number:"))
+for i in range(1,N + 1):        # Loop from 1 to 
+	if i%2 == 0:                # Check if i is even
+		print(i)                # Print even number
