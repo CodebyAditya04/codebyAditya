95e4776bb28fd47e0288ce2827bd743ccd5d2702
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-07-02 23:52:08 +0530

    Add files via upload

---
+++ loops/for_pattern_loop.py
@@ -0,0 +1,4 @@
+for i in range(5):              # outer loop controls rows
+    for j in range(5):          # inner loop controls columns
+        print("*", end=" ")     # print star and stay on same line
+    print()                     # move to the next line after a row
