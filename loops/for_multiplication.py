fc622de75585acacd74fc17d2d6f99d3bd7b2760
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-07-03 00:00:15 +0530

    Add files via upload

---
+++ loops/for_multiplication.py
@@ -0,0 +1,4 @@
+for i in range(1, 6):              # Outer loop: numbers 1 to 5
+    for j in range(1, 11):         # Inner loop: multiplier 1 to 10
+        print(i, "*", j, "=", i * j)
+    print()                        # Blank line after each table
