1ed02ee3f3c3cabc8d3ced06a9d5b54fc339fae0
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-07-25 16:32:23 +0530

    Add files via upload

---
+++ listrecap/even_numbers_comprehension.py
@@ -0,0 +1,5 @@
+# Generate a list of even numbers from 1 to 20 using list comprehension
+even_numbers = [x for x in range(1,21) if x % 2 == 0]
+
+# Print the list of even numbers
+print("Even numbers from 1 to 20 are:", even_numbers)
