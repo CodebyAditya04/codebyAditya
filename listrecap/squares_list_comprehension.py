17831c64953a2e9568e9eaed0a40259c14755e4a
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-07-25 16:20:07 +0530

    Add files via upload

---
+++ listrecap/squares_list_comprehension.py
@@ -0,0 +1,5 @@
+# Using list comprehension to generate squares of numbers from 1 to 10
+squares = [num * num for num in range(1,11)]
+
+# Printing the list of squared numbers
+print("Squares of numbers from 1 to 10 are:", squares)
