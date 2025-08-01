120e8c83775a30f7e120305d0116cfd64e1535f9
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-01 19:10:29 +0530

    Add files via upload

---
+++ strings/string_indexing.py
@@ -0,0 +1,6 @@
+# A variable to store our string as a value
+string = "learning"
+
+print(string[0])       # Print the first character
+print(string[2])       # Print the third character
+print(string[-1])      # Print the last character
