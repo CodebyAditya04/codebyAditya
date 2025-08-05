d84531f671c71f73b25154faab3f5d1f13b8f0dd
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-05 20:54:34 +0530

    Add files via upload

---
+++ strings/replace_limited_and_all.py
@@ -0,0 +1,7 @@
+text = "Python is fun. Python is powerful. Python is popular."
+
+print("The original text:", text)
+
+new_text = text.replace("Python","Java", 1).replace("is","was")
+
+print("Text after using replace:", new_text)
