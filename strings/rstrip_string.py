420f8e76eebb61878254b428287d53a2502f8fcb
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-05 20:17:02 +0530

    Add files via upload

---
+++ strings/rstrip_string.py
@@ -0,0 +1,4 @@
+text = "   I am learning python   "
+
+print("Before rstrip", repr(text))                # Shows the original text stored in the variable as it is
+print("After rstrip", repr(text.rstrip()))        # Removes spaces from the right side only
