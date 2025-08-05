71c7abc79e319bb7117a00eae32ed9e3ea026716
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-05 20:10:02 +0530

    Add files via upload

---
+++ strings/lstrip_string.py
@@ -0,0 +1,4 @@
+text = "   I am learning python   "
+
+print("Before lstrip", repr(text))                    # Shows the exact value stored in variable
+print("After lstip", repr(text.lstrip()))             # Removes spaces from the left side only 
