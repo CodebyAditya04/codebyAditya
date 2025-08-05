c5bdfa9d7223682c626d9a835621f9f0af38c55c
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-05 20:26:12 +0530

    Add files via upload

---
+++ strings/replace_string.py
@@ -0,0 +1,11 @@
+# Original string
+text = "I am learning Python"
+
+# Display the string before replacement
+print("Before using replace:", text)
+
+# Replace the word "Python" with "Java" and store it in a new variable
+new_text = text.replace("Python","Java")
+
+# Display the modified string
+print("After using replace:", new_text)
