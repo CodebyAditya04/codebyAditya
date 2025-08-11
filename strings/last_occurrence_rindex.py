0d6f5dd6f21bffe434b6d6d38dfc19b79cf950ac
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-11 22:52:34 +0530

    Add files via upload

---
+++ strings/last_occurrence_rindex.py
@@ -0,0 +1,8 @@
+# Define the text to search in
+text = "I love to code in Python because code is fun."
+
+# Find the last occurrence of the substring "code"
+pos = text.rindex("code")
+
+# Display the position of the last occurrence
+print("The last occurrence of the word 'code' in the sentence:", pos)
