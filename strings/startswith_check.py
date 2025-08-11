4eb88ce4baba23c40a604b9d562184ba48d0aa1c
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-11 22:55:58 +0530

    Add files via upload

---
+++ strings/startswith_check.py
@@ -0,0 +1,8 @@
+# Define the text to check
+text = "Learning Python is exciting and rewarding."
+
+# Check if the text starts with "Learning"
+result = text.startswith("Learning")
+
+# Display the result
+print("Does the text start with 'Learning'?:", result)
