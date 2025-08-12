61b76d25a1552e9911b86845826346f6009529bf
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-12 20:40:31 +0530

    Add files via upload

---
+++ strings/endswith_basic.py
@@ -0,0 +1,8 @@
+# Define the text
+text = "Learning Python is fun and rewarding"
+
+# Check if the text ends with the word "rewarding"
+pos = text.endswith("rewarding")
+
+# Display the result (True if it ends with "rewarding", False otherwise)
+print("Does the text end with 'rewarding'?:", pos)
