ae767ac323d3ca8914a26c1b7cf5709cd1da2fce
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-14 00:14:41 +0530

    Add files via upload

---
+++ strings/zfill_string_method.py
@@ -0,0 +1,7 @@
+# Demonstrates the use of the zfill() method in Python
+
+# A numeric string
+num = "95"
+
+# Pad the string with leading zeros until its total length is 5 characters
+print(num.zfill(5))            # Output: 00095
