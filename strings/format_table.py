e336a9f43f4b3a2d454760767b709edc63fb30e9
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-17 15:53:50 +0530

    Add files via upload

---
+++ strings/format_table.py
@@ -0,0 +1,9 @@
+# Formatting a table using center(), ljust(), and rjust()
+
+# Table headers
+print("Item".ljust(15), "Price".rjust(10), "Quantity".center(10))
+
+# Data rows
+print("Apples".ljust(15), "120".rjust(10), "5".center(10))
+print("Bananas".ljust(15), "60".rjust(10), "12".center(10))
+print("Mangoes".ljust(15), "150".rjust(10), "7".center(10))
