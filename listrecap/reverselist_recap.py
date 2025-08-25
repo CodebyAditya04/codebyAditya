50481b4152fa49665578999e3dacc3219a545c29
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-08-25 23:12:22 +0530

    Add files via upload

---
+++ listrecap/reverselist_recap.py
@@ -0,0 +1,9 @@
+# Reverse a list without using reverse()
+numbers = [1, 2, 3, 4, 5]
+reversed_list = []
+
+for i in range(len(numbers) - 1, -1, -1):
+    reversed_list.append(numbers[i])
+
+print("Original List:", numbers)
+print("Reversed List:", reversed_list)
