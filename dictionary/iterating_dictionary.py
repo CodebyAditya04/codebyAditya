4b51913151252cc680321be66441f830ff61247e
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-09-02 19:48:55 +0530

    Add files via upload

---
+++ dictionary/iterating_dictionary.py
@@ -0,0 +1,14 @@
+students = {"Aditya":100, "Aditi":98, "Rohit":99, "kamal":96}
+
+# Looping over keys
+for key in students:
+    print("Looping over keys:", key)
+
+# Looping over values
+for value in students.values():
+    print("Looping over values:", value)
+
+# Looping over key-value pairs
+for key, value in students.items():
+    print("\n Looping over key-value pairs in dictionary:",)
+    print(key, ":", value)
