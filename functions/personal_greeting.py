ec6d2996924644b395185f59440ea009d7cb6ec0
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-09-13 19:26:11 +0530

    Add files via upload

---
+++ functions/personal_greeting.py
@@ -0,0 +1,7 @@
+# Define a function named greet_user that takes one parameter 'name'
+def greet_user(name):
+    # Print a greeting message using the input name
+    print("Hello", name,"👋 Welcome to python practice")
+
+greet_user("Aditya")            # Call the function with "Aditya" as input
+greet_user("Aditi")             # Call the function with "Aditi" as input
