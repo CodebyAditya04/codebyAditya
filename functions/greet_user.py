11a9b0d870a4e35df8115b044c8419221bab2f52
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-09-13 19:18:36 +0530

    Add files via upload

---
+++ functions/greet_user.py
@@ -0,0 +1,7 @@
+# Defining a function called greet
+def greet():
+    # The task that the function performs
+    print("Hello!👋 Welcome back to python")
+
+# Calling the function (this runs the code inside it)
+greet()
