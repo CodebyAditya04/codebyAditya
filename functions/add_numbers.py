a3549e64ec81555153bd9465b1eceb29ae17b8ce
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2025-09-13 19:32:55 +0530

    Add files via upload

---
+++ functions/add_numbers.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return(a + b)             # a and b → placeholders for the numbers you want to add.
+
+# Variable to store the result value in
+result = add(20, 40)
+
+# To display the result
+print("The sum is:", result)
