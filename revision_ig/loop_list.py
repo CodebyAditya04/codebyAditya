4c4d916bb8a676b0fc8d7948681e8651f46653b3
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2026-02-12 18:28:24 +0530

    Add files via upload

---
+++ revision_ig/loop_list.py
@@ -0,0 +1,4 @@
+numbers = [3, 6, 9]
+
+for x in numbers:       # x is a temprary variable
+    print(x)            # on each loop iteration python takes one element, assigns it to x and executes the body of the loop
