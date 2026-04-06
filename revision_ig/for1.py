c75633b4e04de987748696ff234d9f3a66278104
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2026-04-06 19:51:55 +0530

    Add files via upload

---
+++ revision_ig/for1.py
@@ -0,0 +1,3 @@
+for i in range(1, 21):                       # Loop from 1 to 20(range is end exclusive)
+    if i % 2 == 0:                           # If value in loop variable i is divisible by 2 then it is even
+        print("The even numbers are:", i)    # Print the even numbers from 1 to 20
