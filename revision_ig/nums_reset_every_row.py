d97d51182b6758c6f9917991716532aaaee3dc3e
Aditya Sharma <163147171+CodebyAditya04@users.noreply.github.com>
2026-05-24 18:44:47 +0530

    Add files via upload

---
+++ revision_ig/nums_reset_every_row.py
@@ -0,0 +1,4 @@
+for i in range(1, 5):                # Outer loop controls rows
+    for j in range(1, i + 1):        # Print numbers from 1 up to i
+        print(j, end = " ")          # Print j on same line
+    print()                          # Move to next line
